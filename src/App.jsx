fe63456acec7c2eee78eedbef0e6ee699b991c41
Krishna Upadhyay <krishnkant.sjp2004@gmail.com>
2025-09-25 00:22:07 +0530

    some update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 
 import Board from './components/Board'
-import Newboard from './components/newBoard'
+
 
 function App() {
     return(
       <>
-     {/* <Newboard/> */}
+
      <Board/>
       </>
     )
